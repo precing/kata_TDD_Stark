f46c476f3057c2e8a0f51ec1779169f30f5bd381
matias01 <mat16@live.cl>
2016-05-04 12:15:29 -0300

    caso 1 prueva fallida

--- src.agil.java/cl/ubb/agil/java/Stark.java
+++ src.agil.java/cl/ubb/agil/java/Stark.java
@@ -2,6 +2,12 @@ package cl.ubb.agil.java;
 
 public class Stark {
 	
+	private int[] pil;
+	
+	public Stark(){	
+		pil = new int[5];
+	}
+	
 	
 
 }
--- src.agil.test.java/cl/ubb/agil/test/java/TestStark.java
+++ src.agil.test.java/cl/ubb/agil/test/java/TestStark.java
@@ -1,8 +1,10 @@
 package cl.ubb.agil.test.java;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
-import src.agil.java.Stark;
+import cl.ubb.agil.java.Stark;
 
 
 public class TestStark {
@@ -10,5 +12,11 @@ public class TestStark {
 	@Test
 	public void canCreateStark(){
 		Stark pila= new Stark();
+	}
+	
+	@Test 
+	public void newStarkBasio(){
+		Stark pila=new Stark();
+		assertEquals(null, pila.pop());
 	}
 }
